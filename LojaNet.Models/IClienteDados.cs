b4d075961b4a22b4c3bd9a0cd1c22f8b9760134b
Luiz Henrique <luiz04faria@gmail.com>
2025-05-19 19:54:47 -0300

    Adicionar arquivos de projeto.

---
+++ LojaNet.Models/IClienteDados.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaNet.Models
+{
+    public interface IClienteDados
+    {
+        void Incluir (Cliente cliente);
+
+        void Alterar (Cliente cliente);
+
+        void Excluir (Cliente cliente);
+
+            List<Cliente> ObterTodos ();
+        
+        Cliente ObterPorID (string id);
+
+        Cliente ObterPorEmail (string Email);
+
+    }
+}
---
+++ LojaNet.Models/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaNet.Models
+{
+    public class Produto
+    {
+        public string Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public int estoque { get; set; }
+
+
+
+
+    }
+}
