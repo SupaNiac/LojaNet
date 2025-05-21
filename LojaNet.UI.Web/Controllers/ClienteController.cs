9f792067a3df70ab83d70da7ca21ec58b24a4011
Luiz Henrique <luiz04faria@gmail.com>
2025-05-20 21:27:32 -0300

    Adicionado divs, classes, inputs e ValidationSummary dentro do "Incluir.cshtml"

--- LojaNet.UI.Web/Controllers/ClienteController.cs
+++ LojaNet.UI.Web/Controllers/ClienteController.cs
@@ -1,9 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Web;
+using LojaNet.Models;
+
 
 namespace LojaNet.UI.Web.Controllers
 {
     public class ClienteController : Controller
     {
+
+        public ActionResult Incluir()
+        {
+            var cli = new Cliente();
+
+               return View(cli);
+        }
+
         public IActionResult Index()
         {
             return View();
